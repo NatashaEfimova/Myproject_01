aba9125d82cd5beeaed19fb1147bd306f992a904
NatashaEfimova <102250449+NatashaEfimova@users.noreply.github.com>
2022-07-04 20:12:18 +0300

    Create Main Java

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+        int a =5;
+        if (a==5){
+            switch (a){
+                case 5:
+                    System.out.println(a);
+            }
+        }
+    }
+}
